a7d99956297f2cb1bb8675d07032b2c475e5eb9e
Olena Ageyeva <eageeva@hotmail.com>
2018-07-06 12:51:32 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,15 +4,15 @@ class ProductsController < ApplicationController
     #  render json: @product.to_json(only: [:name, :description, :indevtory, :id])
   end
 
-  def inventory
-    product = Product.find(params[:id])
-    render plain: product.inventory > 0 ? true : false
-  end
+  #def inventory
+  #  product = Product.find(params[:id])
+  #  render plain: product.inventory > 0 ? true : false
+  #end
 
-  def description
-    product = Product.find(params[:id])
-    render plain: product.description
-  end
+  #def description
+  #  product = Product.find(params[:id])
+  #  render plain: product.description
+  #end
 
   def new
     @product = Product.new
